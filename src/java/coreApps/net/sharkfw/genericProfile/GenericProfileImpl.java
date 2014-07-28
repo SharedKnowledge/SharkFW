cb02466b5e1e498969f3a4f1d79dd35851fe0a31
BarretHTW <bosche@hotmail.de>
2014-07-28 15:26:25 +0200

    Added comments for the GenericProfileImpl.java

--- src/java/coreApps/net/sharkfw/genericProfile/GenericProfileImpl.java
+++ src/java/coreApps/net/sharkfw/genericProfile/GenericProfileImpl.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package net.sharkfw.genericProfile;
 
 import java.util.Iterator;
@@ -16,30 +13,60 @@ import net.sharkfw.knowledgeBase.SharkKBException;
 import net.sharkfw.knowledgeBase.inmemory.InMemoSharkKB;
 import net.sharkfw.peer.SharkEngine;
 
-/**
+/**  
+ * This class implements the GenericProfile.
+ * 
+ * Generic Profiles are stored within a Shark knowledge base. 
+ * 
+ * Each generic profile has an own ContextPoint, the profileCP. The profileCP
+ * enables the generic profile to store generic data (byte arrays) with keys
+ * as anchors. Through the keys the generic information can be accessed. 
+ * 
+ * Not only generic information, but also interests can be stored by a generic 
+ * Profile. Each interest is an additional ContextPoint of the generic Profile,
+ * but only the profileCP can store generic information.
+ * 
+ * Of course, generic information and interests of the generic Profile can also
+ * be removed.
+ * 
+ * generic information of the generic Profile can reveice an Expose Status. 
+ * This status declares which information can be send to other peers ans which
+ * shall not be send.
  *
- * @author s0540042 dufe & resh
+ * @author dufe (540042) & resc (539752)
  */
-class GenericProfileImpl implements GenericProfile {
+public class GenericProfileImpl implements GenericProfile {
 
-    //private  Iterator<ContextPoint> infoSet;
+    /** Generic Information is stored underneath a ContextPoint, the profileCP */
     private ContextPoint profileCP;
     //protected final SharkEngine se;
+    /** Generic Profiles are stored in a Shark knowledge base */
     protected final SharkKB kb;
-
+    /** Generates a generic profile */
     public GenericProfileImpl(SharkKB kb) throws SharkKBException {
         
         this.kb = kb;
 
     }
-
+/**
+ * Adds an interest to the generic profile.
+ * 
+ * @param interest  the interest which shall be added
+ * @throws SharkKBException 
+ */
     @Override
     public void addInterest(ContextCoordinates interest) throws SharkKBException {
         if (interest != null) {
             kb.createInterest(interest);
         }
     }
-
+    
+/**
+ * Removes an interest from the generic profile.
+ * 
+ * @param interest the interest which shall be removed
+ * @throws SharkKBException 
+ */
     @Override
     public void removeInterest(ContextCoordinates interest) throws SharkKBException {
         if (interest != null) {
@@ -48,6 +75,13 @@ class GenericProfileImpl implements GenericProfile {
         }
     }
 
+    /**
+     * Receives an interest from the genric profile.
+     * 
+     * @param interest the interest which shall be received
+     * @return the requested interest or null
+     * @throws SharkKBException 
+     */
     public ContextPoint getInterest(ContextCoordinates interest) throws SharkKBException {
         if (interest != null) {
             return kb.getContextPoint(interest);
@@ -57,7 +91,13 @@ class GenericProfileImpl implements GenericProfile {
         }
     }
     
-    //SCHLUESSEL?
+    /**
+     * Adds generic information to the generic profile.
+     * 
+     * @param key defines with which key the information can later be received or removed. The key has to be unique! 
+     * @param daten the generic information which shall be stored
+     * @throws SharkKBException 
+     */
     @Override
     public void addInformation(String key, byte[] daten) throws SharkKBException {
         if (key != null && daten != null &&  (profileCP.getInformation(key) == null)) {
@@ -65,6 +105,13 @@ class GenericProfileImpl implements GenericProfile {
         }
     }
 
+    /**
+     * Receives generic information from the generic profile.
+     * 
+     * @param key defines which information shall be received
+     * @return the requested information or null
+     * @throws SharkKBException 
+     */
     @Override
     public byte[] getInformation(String key) throws SharkKBException {
         if (key != null && (profileCP.getInformation(key) != null)) {
@@ -76,6 +123,12 @@ class GenericProfileImpl implements GenericProfile {
         }
     }
 
+    /**
+     * Removes generic information from the generic profile
+     * 
+     * @param key defines which information shall be removed
+     * @throws SharkKBException 
+     */
     @Override
     public void removeInformation(String key) throws SharkKBException {
         if (key != null ) {
@@ -83,14 +136,32 @@ class GenericProfileImpl implements GenericProfile {
         }
     }
 
+    /**
+     * Sets an expose status for generic information of the generic profile.
+     * The expose status determines whether the information can be send to other
+     * peers or not.
+     * 
+     * @param keys defines which pieces' of information expose status shall be changed
+     * @param ExposeStatus 0 = can not be sent to ohter peers, 1 = can be sent to ohter peers
+     * @param peers defines which other peers' expose status of the selected pieces of information shall be changed.
+     * @throws SharkKBException 
+     */
     @Override
     public void setExposeStatus(String[] keys, int ExposeStatus, Iterator<PeerSemanticTag> peers) throws SharkKBException {
-        //Access?
+        //TODO
     }
 
+    /**
+     * Returns an iterator of peers which are allowed to receive the information which
+     * is stored beneath the given key.
+     * 
+     * @param key defines which information is reckoned.
+     * @return an iterator of peers which are allowed to receive the information which is stored beneath the given key.
+     * @throws SharkKBException 
+     */
     @Override
     public Iterator<PeerSemanticTag> getAllowedPeers(String key) throws SharkKBException {
-        //Access?
+        //TODO
         return null;
     }
 
