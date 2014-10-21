06a81aae30b55515c957d9394e690458866d46b4
Simon Arnold <s0539710@htw-berlin.de>
2014-10-21 22:44:52 +0200

    Added SyncContextPoint class declaration and test method names. No implementation yet though. Not much time today :/

---
+++ src/java/core/net/sharkfw/knowledgeBase/sync/SyncContextPoint.java
@@ -0,0 +1,127 @@
+package net.sharkfw.knowledgeBase.sync;
+
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.Iterator;
+import net.sharkfw.knowledgeBase.ContextCoordinates;
+import net.sharkfw.knowledgeBase.ContextPoint;
+import net.sharkfw.knowledgeBase.ContextPointListener;
+import net.sharkfw.knowledgeBase.Information;
+
+/**
+ *
+ * @author simon
+ */
+public class SyncContextPoint implements ContextPoint {
+
+    @Override
+    public Information addInformation() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addInformation(Information source) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Information addInformation(InputStream is, long len) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Information addInformation(byte[] content) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Information addInformation(String content) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Enumeration<Information> enumInformation() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Iterator<Information> getInformation(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Iterator<Information> getInformation() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeInformation(Information toDelete) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ContextCoordinates getContextCoordinates() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setContextCoordinates(ContextCoordinates cc) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getNumberInformation() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setListener(ContextPointListener cpl) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeListener() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setSystemProperty(String name, String value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getSystemProperty(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setProperty(String name, String value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getProperty(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setProperty(String name, String value, boolean transfer) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeProperty(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Enumeration<String> propertyNames() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Enumeration<String> propertyNames(boolean all) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
---
+++ src/java/j2seTests/net/sharkfw/knowledgeBase/sync/SyncContextPointTests.java
@@ -0,0 +1,80 @@
+package net.sharkfw.knowledgeBase.sync;
+
+
+// JUnit imports
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+/**
+ *  @author Simon Arnold <s0539710@htw-berlin.de>
+ *  @author Veit Heller <veit@veitheller.de>
+ */
+public class SyncContextPointTests {
+    
+    public SyncContextPointTests() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testCreateNewSyncContextPoint() {
+        
+    }
+    
+    @Test
+    // Needed? Lets see
+    public void testCreateSyncContextPointFromExisting() {
+        
+    }
+    
+    @Test
+    public void testGetSyncContextPointInformation() {
+        
+    }
+    
+    @Test
+    public void testAddInformation() {
+        
+    }
+    
+    @Test
+    public void testRemoveInformation() {
+        
+    }
+    
+    @Test
+    public void testGetVersion() {
+        
+    }
+    
+    @Test
+    public void testIncrementedVersionByAddingInformation() {
+        
+    }
+    
+    @Test
+    public void testIncrementedVersionByRemovingInformation() {
+        
+    }
+    
+}
