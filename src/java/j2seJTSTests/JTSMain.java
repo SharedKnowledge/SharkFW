3f75fa7d918e99a3a609d2e3ac80529900574552
MrMong <t90zug@gmx.de>
2014-11-06 14:53:26 +0100

    add new JTS-Test-Folder

---
+++ src/java/j2seJTSTests/JTSMain.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author s0542709
+ */
+public class JTSMain
+{
+  
+  public JTSMain()
+  {
+  }
+  
+  @BeforeClass
+  public static void setUpClass()
+  {
+  }
+  
+  @AfterClass
+  public static void tearDownClass()
+  {
+  }
+  
+  @Before
+  public void setUp()
+  {
+  }
+  
+  @After
+  public void tearDown()
+  {
+  }
+
+    // TODO add test methods here.
+  // The methods must be annotated with annotation @Test. For example:
+  //
+  // @Test
+  // public void hello() {}
+}
