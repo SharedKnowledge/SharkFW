c7a979af4890d9822d918e7ddfad4c1279c224d8
Thomas Schwotzer <thomas.schwotzer@htw-berlin.de>
2014-11-07 18:23:28 +0100

    we need a system wide factory - start implementing a dummy spatial algebra that is used on devices with no JTS support

---
+++ src/java/core/net/sharkfw/knowledgeBase/geom/SpatialAlgebra.java
@@ -0,0 +1,15 @@
+package net.sharkfw.knowledgeBase.geom;
+
+/**
+ * That class contains (dummy) implementations
+ * of spatial operation. The real implementation
+ * on J2SE is made by means of Java Topology Suite.
+ * 
+ * That class will be used on every device which
+ * can't or don't want to use JTS.
+ * 
+ * @author thsc
+ */
+public class SpatialAlgebra {
+    
+}
