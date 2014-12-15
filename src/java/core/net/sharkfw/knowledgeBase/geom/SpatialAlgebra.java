c56d8d1da6017fb6ab9a6724ca0fe4b06a0391ad
Thomas Schwotzer <thomas.schwotzer@htw-berlin.de>
2014-12-15 09:17:32 +0100

    start integrating spatial algebra into SharkCSAlgebra

--- src/java/core/net/sharkfw/knowledgeBase/geom/SpatialAlgebra.java
+++ src/java/core/net/sharkfw/knowledgeBase/geom/SpatialAlgebra.java
@@ -1,9 +1,13 @@
 package net.sharkfw.knowledgeBase.geom;
 
+import net.sharkfw.knowledgeBase.SpatialSemanticTag;
+import net.sharkfw.system.L;
+
 /**
- * That class contains (dummy) implementations
- * of spatial operation. The real implementation
- * on J2SE is made by means of Java Topology Suite.
+ * That class is just a placeholder. It allows 
+ * defining a spatial algebra in SharkCSAlgebra.
+ * The actual implementation with 
+ * J2SE is made by means of Java Topology Suite.
  * 
  * That class will be used on every device which
  * can't or don't want to use JTS.
@@ -12,4 +16,35 @@ package net.sharkfw.knowledgeBase.geom;
  */
 public class SpatialAlgebra {
     
+    private static SpatialAlgebra spatialAlgebra = null;
+    
+    public static SpatialAlgebra getSpatialAlgebra() {
+        if(SpatialAlgebra.spatialAlgebra != null) {
+            return SpatialAlgebra.spatialAlgebra;
+        }
+        
+        try {
+            Class spatialAlgebraClass = Class.forName("net.sharkfw.knowledgeBase.geom.jts.SpatialAlgebra");
+            SpatialAlgebra.spatialAlgebra = (SpatialAlgebra) spatialAlgebraClass.newInstance();
+            
+        } catch (Exception ex) {
+            // class cannot be found or instantiated
+            L.d("cannot instanciate JTS spatial algebra - that platform probably does not support JTS. That's ok.", ex.getMessage());
+        }
+        
+        if(SpatialAlgebra.spatialAlgebra == null) {
+            // set up default dummy implementation
+            SpatialAlgebra.spatialAlgebra = new SpatialAlgebra();
+        }
+        
+        return SpatialAlgebra.spatialAlgebra;
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+    //           very optimistic dummy implementation start here.         //
+    ////////////////////////////////////////////////////////////////////////
+    
+    public boolean identical(SpatialSemanticTag sTagA, SpatialSemanticTag sTagB) {
+        return true;
+    }
 }
