81e5c2398b4ef5d658364078c3e5e8999eda5bb8
Thomas Schwotzer <thomas.schwotzer@htw-berlin.de>
2015-02-20 14:06:13 +0100

    removed unused imports

--- src/java/j2se_android/net/sharkfw/knowledgeBase/filesystem/FSContextPoint.java
+++ src/java/j2se_android/net/sharkfw/knowledgeBase/filesystem/FSContextPoint.java
@@ -6,8 +6,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Enumeration;
 import java.util.Vector;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import net.sharkfw.kep.format.XMLSerializer;
 import net.sharkfw.knowledgeBase.ContextCoordinates;
 import net.sharkfw.knowledgeBase.Information;
