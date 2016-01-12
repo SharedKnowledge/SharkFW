cef041447a0b68f93490362df5bf6fddfbcb8894
Michael Schwarz <schwarzm90@gmail.com>
2016-01-12 16:36:26 +0100

    Add SemanticTag and SubjectIdentifier.

--- src/java/core/net/sharkfw/sip/SemanticTag.java
+++ src/java/core/net/sharkfw/sip/SemanticTag.java
@@ -5,10 +5,55 @@
  */
 package net.sharkfw.sip;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * @author micha
  */
 public class SemanticTag {
+    
+    public static final String NAME = "NAME";
+    
+    public static final String SI = "SI";
+    
+    public static final String ID = "ID";
+    
+    private String nameField;
+    
+    private List<SubjectIdentifier> subjectIdentifiers;
+    
+    public SemanticTag(String name, SubjectIdentifier identifier){
+        this.nameField = name;
+        this.subjectIdentifiers.add(identifier);
+    }
+    
+    public SemanticTag(String name, LinkedList<SubjectIdentifier> identifiers){
+        this.nameField = name;
+        this.subjectIdentifiers.addAll(identifiers);
+    }
+
+    public String getName() {
+        return nameField;
+    }
+
+    public void setName(String nameField) {
+        this.nameField = nameField;
+    }
+
+    public List<SubjectIdentifier> getSubjectIdentifiers() {
+        return subjectIdentifiers;
+    }
+
+    public void setSubjectIdentifiers(List<SubjectIdentifier> subjectIdentifiers) {
+        this.subjectIdentifiers = subjectIdentifiers;
+    }
+    
+    public void addSubjectIdentifier(SubjectIdentifier identifier) {
+        if(!subjectIdentifiers.contains(identifier)){
+            subjectIdentifiers.add(identifier);
+        }
+    }    
     
 }
