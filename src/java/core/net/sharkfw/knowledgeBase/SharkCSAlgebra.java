7419dfcd31155926951515bad90a9445e51db650
Michael Schwarz <schwarzm90@gmail.com>
2016-02-15 15:17:19 +0100

    Add the SemanticNetHelper from https://github.com/danieldiekmeier/Shark-SemanticNetHelper to the SharkCSAlgebra.

--- src/java/core/net/sharkfw/knowledgeBase/SharkCSAlgebra.java
+++ src/java/core/net/sharkfw/knowledgeBase/SharkCSAlgebra.java
@@ -4,8 +4,10 @@ import net.sharkfw.asip.ASIPSpace;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Stack;
 import java.util.Vector;
 import net.sharkfw.asip.ASIPInterest;
 
@@ -24,6 +26,15 @@ public abstract class SharkCSAlgebra {
     private static net.sharkfw.knowledgeBase.geom.SpatialAlgebra spatialAlgebra;
     private static final String JTS_SPATIAL_ALGEBRA_CLASS = 
             "net.sharkfw.knowledgeBase.geom.jts.SpatialAlgebra";
+    
+    private static ArrayList<ArrayList<SNSemanticTag>> tComponents;
+
+    private static int tIndex;
+    private static Stack<SNSemanticTag> tStack;
+
+    private static HashMap<SNSemanticTag, Integer> tIndices;
+    private static HashMap<SNSemanticTag, Integer> tLowLinks;
+    private static HashMap<SNSemanticTag, Boolean> tChecked;
     
     static {
         
@@ -2443,4 +2454,268 @@ public abstract class SharkCSAlgebra {
         }
         return valid;
     }
+    
+    /**
+    * Checks if the SemanticNet instance is a correct taxonomy
+    * using Tarjan's Strongly Connected Components Algorithm
+    * https://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm
+    *
+    * @param semanticNet A SemanticNet, probably with some SemanticTags that have some kind of connection
+    * @param predicate The predicate that should be checked.
+    *
+    * @return true if the connections of Predicate in the SemanticNet
+    * form a taxonomy, false if there are circular dependencies between the tags
+    */
+   public static boolean isTaxonomy(SemanticNet semanticNet, String predicate) throws SharkKBException {
+
+        // Reset
+        tComponents = new ArrayList<ArrayList<SNSemanticTag>>();
+        tIndex = 0;
+        tStack = new Stack<SNSemanticTag>();
+
+        tIndices = new HashMap<SNSemanticTag, Integer>();
+        tLowLinks = new HashMap<SNSemanticTag, Integer>();
+        tChecked = new HashMap<SNSemanticTag, Boolean>();
+
+        // Return immediately if SemanticNet is empty
+        if (semanticNet.size() == 0) {
+            return true;
+        }
+
+        Iterator<SemanticTag> iter = semanticNet.stTags();
+        ArrayList<SNSemanticTag> tags = new ArrayList<SNSemanticTag>();
+        while (iter.hasNext()) {
+            tags.add((SNSemanticTag) iter.next());
+        }
+
+        for (SNSemanticTag tag : tags) {
+            if (connectsToItself(tag, predicate)) {
+                return false;
+            }
+
+            if (!tIndices.containsKey(tag)) {
+                strongConnect(tag, predicate);
+            }
+        }
+
+        // Check if there are strongly connected components
+        // that are longer than one item
+        int longestListSize = 0;
+        for (ArrayList<SNSemanticTag> list : tComponents) {
+            if (longestListSize < list.size()) {
+                longestListSize = list.size();
+            }
+        }
+
+        return longestListSize <= 1;
+   }
+
+   private static boolean connectsToItself(SNSemanticTag tag, String predicate) {
+        Enumeration<SNSemanticTag> subTags = tag.targetTags(predicate);
+        if (subTags != null) {
+            while(subTags.hasMoreElements()) {
+                SNSemanticTag subTag = subTags.nextElement();
+                if (subTag.equals(tag)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+   }
+
+   private static void strongConnect(SNSemanticTag tag, String predicate) throws SharkKBException {
+        tIndices.put(tag, tIndex);
+        tLowLinks.put(tag, tIndex);
+        tChecked.put(tag, true);
+        tIndex = tIndex + 1;
+        tStack.push(tag);
+
+        Enumeration<SNSemanticTag> subTags = tag.targetTags(predicate);
+        if (subTags != null) {
+            while(subTags.hasMoreElements()) {
+                SNSemanticTag w = subTags.nextElement();
+                if (!tIndices.containsKey(w)) {
+                    strongConnect(w, predicate);
+                    tLowLinks.put(tag, Math.min(tLowLinks.get(tag), tLowLinks.get(w)));
+                } else if (tChecked.containsKey(w)) {
+                    tLowLinks.put(tag, Math.min(tLowLinks.get(tag), tIndices.get(w)));
+                }
+            }
+        }
+
+        if (tLowLinks.get(tag) == tIndices.get(tag)) {
+            ArrayList<SNSemanticTag> component = new ArrayList<SNSemanticTag>();
+            SNSemanticTag w = null;
+            while (w != tag) {
+                w = tStack.pop();
+                tChecked.put(w, false);
+                component.add(w);
+            }
+
+            tComponents.add(component);
+        }
+   }
+
+   /**
+    * Checks if all the connections of predicate in the SemanticNet instance 
+    * form a transitive net.
+    *
+    * @param semanticNet A SemanticNet, probably with some SemanticTags that have some kind of connection
+    * @param predicate The predicate that should be checked.
+    *
+    * @return true if all connections of the Predicate are transitive, false if they are not
+    */
+   public static boolean isTransitive(SemanticNet semanticNet, String predicate) throws SharkKBException {
+        tChecked = new HashMap<SNSemanticTag, Boolean>();
+
+        Iterator<SemanticTag> iter = semanticNet.stTags();
+
+        while (iter.hasNext()) {
+            SNSemanticTag tag = (SNSemanticTag) iter.next();
+            Enumeration<SNSemanticTag> subTags = tag.targetTags(predicate);
+
+            if (!tChecked.containsKey(tag) && subTags != null) {
+                ArrayList<SNSemanticTag> rootSubTags = new ArrayList<SNSemanticTag>();
+                while (subTags.hasMoreElements()) {
+                    rootSubTags.add((SNSemanticTag) subTags.nextElement());
+                }
+
+                if (!transitive(tag, rootSubTags, predicate)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+   }
+
+   private static boolean transitive(SNSemanticTag tag, ArrayList<SNSemanticTag> rootSubTags, String predicate) {
+        tChecked.put(tag, true);
+
+        Enumeration<SNSemanticTag> subTags = tag.targetTags(predicate);
+        if (subTags != null) {
+            while(subTags.hasMoreElements()) {
+                SNSemanticTag w = subTags.nextElement();
+
+                // w is not in the root's sub tags
+                if (!rootSubTags.contains(w)) {
+                    return false;
+                }
+
+                // w has no further children
+                if (w.targetTags(predicate) == null) {
+                    return true;
+                }
+
+                return transitive(w, rootSubTags, predicate);
+            }
+        }
+        return true;
+   }
+
+   /**
+    * @param semanticNet A SemanticNet, probably with some SemanticTags that have some kind of connection
+    * @param predicate The predicate that should be checked.
+    *
+    * @return true if all connections with the predicate are symmetric
+    */
+   public static boolean isSymmetric(SemanticNet semanticNet, String predicate) throws SharkKBException {
+        Iterator<SemanticTag> iter = semanticNet.stTags();
+
+        while (iter.hasNext()) {
+            SNSemanticTag tag = (SNSemanticTag) iter.next();
+
+            Enumeration<SNSemanticTag> subTags = tag.targetTags(predicate);
+
+            if (subTags != null) {
+                while (subTags.hasMoreElements()) {
+                    Enumeration<SNSemanticTag> deeperSubTags = subTags.nextElement().targetTags(predicate);
+
+                    if (deeperSubTags != null) {
+                        ArrayList<SNSemanticTag> deeperSubTagsList = new ArrayList<>();
+                        while (deeperSubTags.hasMoreElements()) {
+                            deeperSubTagsList.add(deeperSubTags.nextElement());
+                        }
+
+                        if (!deeperSubTagsList.contains(tag)) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+   }
+
+   /**
+    * Changes the SemanticNet to make all the connections of predicate symmetric,
+    * that means that every connection that previously only went from A -> B, now also
+    * goes from B -> A.
+    * (The SemanticNet is then guaranteed to pass .isSymmetric for the same predicate.)
+    *
+    * @param semanticNet A SemanticNet, probably with some SemanticTags that have some kind of connection
+    * @param predicate The predicate that should be checked.
+    */
+   public static void makeSymmetric(SemanticNet semanticNet, String predicate) throws SharkKBException {
+        Iterator<SemanticTag> iter = semanticNet.stTags();
+
+        while (iter.hasNext()) {
+            SNSemanticTag tag = (SNSemanticTag) iter.next();
+            Enumeration<SNSemanticTag> subTags = tag.targetTags(predicate);
+
+            if (subTags != null) {
+                while (subTags.hasMoreElements()) {
+                    subTags.nextElement().setPredicate(predicate, tag);
+                }
+            }
+        }
+   }
+
+   /**
+    * Changes the SemanticNet to make all the connections of predicate transitive.
+    * (The SemanticNet is then guaranteed to pass .isTransitive for the same predicate.)
+    *
+    * @param semanticNet A SemanticNet, probably with some SemanticTags that have some kind of connection
+    * @param predicate The predicate that should be checked.
+    */
+   public static void makeTransitive(SemanticNet semanticNet, String predicate) throws SharkKBException {
+        tChecked = new HashMap<>();
+
+        Iterator<SemanticTag> iter = semanticNet.stTags();
+
+        while (iter.hasNext()) {
+            SNSemanticTag tag = (SNSemanticTag) iter.next();
+            Enumeration<SNSemanticTag> subTags = tag.targetTags(predicate);
+
+            if (subTags != null) {
+                ArrayList<SNSemanticTag> rootSubTags = new ArrayList<>();
+                while (subTags.hasMoreElements()) {
+                    rootSubTags.add((SNSemanticTag) subTags.nextElement());
+                }
+
+                makeTransitiveHelper(tag, tag, rootSubTags, predicate);
+            }
+        }
+   }
+
+   private static void makeTransitiveHelper(SNSemanticTag tag, SNSemanticTag rootTag, ArrayList<SNSemanticTag> rootSubTags, String predicate) {
+        Enumeration<SNSemanticTag> subTags = tag.targetTags(predicate);
+        if (subTags != null) {
+            while(subTags.hasMoreElements()) {
+                SNSemanticTag w = subTags.nextElement();
+
+                // w is not in the root's sub tags
+                if (!rootSubTags.contains(w)) {
+                    rootTag.setPredicate(predicate, w);
+                }
+
+                // w has no further children
+                if (w.targetTags(predicate) == null) {
+                    return;
+                }
+
+                makeTransitiveHelper(w, rootTag, rootSubTags, predicate);
+            }
+        }
+   }
 }
---
+++ src/java/j2seTests/ApiRev1/SemanticNetHelperTest.java
@@ -0,0 +1,223 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ApiRev1;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import net.sharkfw.knowledgeBase.SNSemanticTag;
+import net.sharkfw.knowledgeBase.SemanticNet;
+import net.sharkfw.knowledgeBase.SharkCSAlgebra;
+import net.sharkfw.knowledgeBase.SharkKBException;
+import net.sharkfw.knowledgeBase.inmemory.InMemoSemanticNet;
+
+
+public class SemanticNetHelperTest {
+
+	SemanticNet sn;
+
+	@Before
+	public void setUp() throws Exception {
+		sn = new InMemoSemanticNet();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testEmpty() throws SharkKBException {
+		assertTrue(SharkCSAlgebra.isTaxonomy(sn, "super"));
+	}
+
+	@Test
+	public final void testOne() throws SharkKBException {
+		sn.createSemanticTag("Java", "https://en.wikipedia.org/wiki/Java_(programming_language)");
+		assertTrue(SharkCSAlgebra.isTaxonomy(sn, "super"));
+	}
+
+	@Test
+	public final void testTwoWithoutConnection() throws SharkKBException {
+		sn.createSemanticTag("Java", "1");
+		sn.createSemanticTag("Programming Languages", "2");
+		assertTrue(SharkCSAlgebra.isTaxonomy(sn, "super"));
+	}
+
+	@Test
+	public final void testTwoWithOneConnection() throws SharkKBException {
+		SNSemanticTag java = sn.createSemanticTag("Java", "1");
+		SNSemanticTag pl = sn.createSemanticTag("Programming Languages", "2");
+
+		sn.setPredicate(pl, java, "super");
+
+		assertTrue(SharkCSAlgebra.isTaxonomy(sn, "super"));
+	}
+
+	@Test
+	public final void testOneWithConnectionToItself() throws SharkKBException {
+		SNSemanticTag god = sn.createSemanticTag("God", "1");
+		sn.createSemanticTag("Jesus", "2");
+		sn.createSemanticTag("Holy Spirit", "3");
+
+		sn.setPredicate(god, god, "super");
+
+		assertFalse(SharkCSAlgebra.isTaxonomy(sn, "super"));
+	}
+
+	@Test
+	public final void testThreeWithCircularConnection() throws SharkKBException {
+		SNSemanticTag god = sn.createSemanticTag("God", "1");
+		SNSemanticTag jesus = sn.createSemanticTag("Jesus", "2");
+		SNSemanticTag holySpirit = sn.createSemanticTag("Holy Spirit", "3");
+
+		sn.setPredicate(god, jesus, "super");
+		sn.setPredicate(jesus, holySpirit, "super");
+		sn.setPredicate(holySpirit, god, "super");
+
+		assertFalse(SharkCSAlgebra.isTaxonomy(sn, "super"));
+	}
+
+	@Test
+	public final void testLargeNetWithManyCircularConnections() throws SharkKBException {
+		// Example from here:
+		// https://github.com/bwesterb/py-tarjan/blob/350f646c86556fd76d26c0a413ca34375371c60b/README.rst
+
+		SNSemanticTag st1 = sn.createSemanticTag("1", "1");
+		SNSemanticTag st2 = sn.createSemanticTag("2", "2");
+		SNSemanticTag st3 = sn.createSemanticTag("3", "3");
+		SNSemanticTag st4 = sn.createSemanticTag("4", "4");
+		SNSemanticTag st5 = sn.createSemanticTag("5", "5");
+		SNSemanticTag st6 = sn.createSemanticTag("6", "6");
+		SNSemanticTag st7 = sn.createSemanticTag("7", "7");
+		SNSemanticTag st8 = sn.createSemanticTag("8", "8");
+		SNSemanticTag st9 = sn.createSemanticTag("9", "9");
+
+		sn.setPredicate(st1, st2, "super");
+		sn.setPredicate(st2, st1, "super");
+		sn.setPredicate(st2, st5, "super");
+		sn.setPredicate(st3, st4, "super");
+		sn.setPredicate(st4, st3, "super");
+		sn.setPredicate(st4, st5, "super");
+		sn.setPredicate(st5, st6, "super");
+		sn.setPredicate(st6, st7, "super");
+		sn.setPredicate(st7, st8, "super");
+		sn.setPredicate(st8, st6, "super");
+		sn.setPredicate(st8, st9, "super");
+
+		assertFalse(SharkCSAlgebra.isTaxonomy(sn, "super"));
+	}
+
+	@Test
+	public final void testTransitiveFalse() throws SharkKBException {
+		SNSemanticTag a = sn.createSemanticTag("a", "1");
+		SNSemanticTag b = sn.createSemanticTag("b", "2");
+		SNSemanticTag c = sn.createSemanticTag("c", "3");
+
+		sn.setPredicate(a, b, "super");
+		sn.setPredicate(b, c, "super");
+
+		assertFalse(SharkCSAlgebra.isTransitive(sn, "super"));
+	}
+
+	@Test
+	public final void testTransitiveTrue() throws SharkKBException {
+		SNSemanticTag a = sn.createSemanticTag("a", "1");
+		SNSemanticTag b = sn.createSemanticTag("b", "2");
+		SNSemanticTag c = sn.createSemanticTag("c", "3");
+
+		SNSemanticTag d = sn.createSemanticTag("d", "4");
+		SNSemanticTag e = sn.createSemanticTag("e", "5");
+
+		sn.setPredicate(a, b, "super");
+		sn.setPredicate(b, c, "super");
+		sn.setPredicate(a, c, "super");
+
+		sn.setPredicate(e, d, "super");
+
+		assertTrue(SharkCSAlgebra.isTransitive(sn, "super"));
+	}
+	
+	@Test
+	public final void testSymmetricTrue() throws SharkKBException {
+		SNSemanticTag a = sn.createSemanticTag("a", "1");
+		SNSemanticTag b = sn.createSemanticTag("b", "2");
+		SNSemanticTag c = sn.createSemanticTag("c", "3");
+		SNSemanticTag d = sn.createSemanticTag("d", "4");
+		
+		sn.setPredicate(a, b, "super");
+		sn.setPredicate(b, a, "super");
+		sn.setPredicate(b, d, "super");
+		sn.setPredicate(d, b, "super");
+		
+		assertTrue(SharkCSAlgebra.isSymmetric(sn, "super"));
+	}
+	
+	@Test
+	public final void testSymmetricFalse() throws SharkKBException {
+		SNSemanticTag a = sn.createSemanticTag("a", "1");
+		SNSemanticTag b = sn.createSemanticTag("b", "2");
+		SNSemanticTag c = sn.createSemanticTag("c", "3");
+		SNSemanticTag d = sn.createSemanticTag("d", "4");
+		
+		sn.setPredicate(a, b, "super");
+		sn.setPredicate(b, d, "super");
+		sn.setPredicate(d, b, "super");
+		
+		assertFalse(SharkCSAlgebra.isSymmetric(sn, "super"));
+	}
+	
+	@Test
+	public final void testMakeTransitive() throws SharkKBException {
+		SNSemanticTag a = sn.createSemanticTag("a", "1");
+		SNSemanticTag b = sn.createSemanticTag("b", "2");
+		SNSemanticTag c = sn.createSemanticTag("c", "3");
+
+		sn.setPredicate(a, b, "super");
+		sn.setPredicate(b, c, "super");
+		
+		SharkCSAlgebra.makeTransitive(sn, "super");
+		
+		assertTrue(SharkCSAlgebra.isTransitive(sn, "super"));
+		   
+		ArrayList<SNSemanticTag> subTagsList = new ArrayList<>();
+		Enumeration<SNSemanticTag> subTags = a.targetTags("super");
+		while (subTags.hasMoreElements()) {
+			subTagsList.add(subTags.nextElement());
+		}
+		assertTrue(subTagsList.contains(c));
+	}
+	
+	@Test
+	public final void testMakeSymmetric() throws SharkKBException {
+		SNSemanticTag a = sn.createSemanticTag("a", "1");
+		SNSemanticTag b = sn.createSemanticTag("b", "2");
+		SNSemanticTag c = sn.createSemanticTag("c", "3");
+		SNSemanticTag d = sn.createSemanticTag("d", "4");
+		
+		sn.setPredicate(a, b, "super");
+		sn.setPredicate(b, c, "super");
+		sn.setPredicate(c, d, "super");
+		
+		SharkCSAlgebra.makeSymmetric(sn, "super");
+		
+		assertTrue(SharkCSAlgebra.isSymmetric(sn, "super"));
+		
+		ArrayList<SNSemanticTag> subTagsList = new ArrayList<>();
+		Enumeration<SNSemanticTag> subTags = b.targetTags("super");
+		while (subTags.hasMoreElements()) {
+			subTagsList.add(subTags.nextElement());
+		}
+		assertTrue(subTagsList.contains(a));
+	}
+
+}
