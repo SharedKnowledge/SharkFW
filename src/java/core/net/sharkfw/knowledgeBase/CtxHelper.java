0e3f7682b4d25ec9e885de4bc16706eac2c2be8e
Thomas Schwotzer <thomas.schwotzer@htw-berlin.de>
2016-02-23 12:55:22 +0100

    add little helper class

---
+++ src/java/core/net/sharkfw/knowledgeBase/CtxHelper.java
@@ -0,0 +1,45 @@
+package net.sharkfw.knowledgeBase;
+
+import net.sharkfw.knowledgeBase.inmemory.InMemoSharkKB;
+
+/**
+ *
+ * @author thsc
+ */
+class CtxHelper {
+    public STSet result;
+    public boolean success; 
+        
+    public CtxHelper contextualizeSingleDimension(STSet source, 
+            STSet context, FragmentationParameter fp) 
+                throws SharkKBException {
+
+        // test on any
+        if(SharkCSAlgebra.isAny(source)) {
+            // source is any - return copy of context
+            this.result = InMemoSharkKB.createInMemoCopy(context);
+            this.success = true; // in any case thats a valid result
+        } else {
+            if(SharkCSAlgebra.isAny(context)) {
+                // source is not any but context - return source
+                this.result = InMemoSharkKB.createInMemoCopy(source);
+                this.success = true;
+            } else {
+                // source and context are not any - contextualize
+                this.result = source.contextualize(context, fp);
+                // that must be not null if mutual interest exists
+                if(this.result == null || this.result.isEmpty()) {
+                    this.success = false;
+                }
+            }
+        }
+
+        // don' wast space with empty sets
+        if(this.result != null && this.result.isEmpty()) {
+            this.result = null;
+        }
+
+        // target is not null and not empty
+        return this;
+    }
+}
