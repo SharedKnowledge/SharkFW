f23dfe35b5d37d5163e07a8a5146e9ed6b5a5059
Thomas Schwotzer <thomas.schwotzer@htw-berlin.de>
2016-02-23 08:05:51 +0100

    add test class for ASIP compliant in memo sharkkb

---
+++ src/java/j2seTests/ASIPTests/ASIPSharkKBImpl.java
@@ -0,0 +1,27 @@
+package ASIPTests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author thsc
+ */
+public class ASIPSharkKBImpl {
+    
+    public ASIPSharkKBImpl() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void hello() {}
+}
