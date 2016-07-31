a0db8b6683c2d14b3b66c1d877dee2b71531e92a
thsc <thsc@Decius.Speedport_W_723V_1_40_000>
2016-07-31 20:33:46 +0200

    first working (but hardly tested) sync kb implementation.

--- src/main/java/net/sharkfw/knowledgeBase/sync/SyncKP.java
+++ src/main/java/net/sharkfw/knowledgeBase/sync/SyncKP.java
@@ -2,92 +2,95 @@ package net.sharkfw.knowledgeBase.sync;
 
 import net.sharkfw.asip.engine.ASIPConnection;
 import net.sharkfw.asip.engine.ASIPInMessage;
-import net.sharkfw.knowledgeBase.*;
 import net.sharkfw.peer.ContentPort;
 import net.sharkfw.peer.SharkEngine;
 
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.sharkfw.asip.engine.ASIPSerializer;
+import net.sharkfw.knowledgeBase.PeerSTSet;
+import net.sharkfw.knowledgeBase.PeerSemanticTag;
+import net.sharkfw.knowledgeBase.SharkKBException;
+import net.sharkfw.knowledgeBase.inmemory.InMemoSharkKB;
+import net.sharkfw.system.L;
+import org.json.JSONException;
 
 /**
  * Created by j4rvis on 19.07.16.
+ * 
+ * @author thsc
  */
-public class SyncKP extends ContentPort implements KnowledgeBaseListener{
-    public SyncKP(SharkEngine se) {
+public class SyncKP extends ContentPort {
+    private PeerSTSet allowedUsers = null;
+    private SyncKB syncKB;
+    private HashMap<PeerSemanticTag, Long> lastSeen = new HashMap<>();
+    private static final String LAST_SEEN_PROPERTY_NAME = "SHARK_SYNC_LAST_SEEN";
+    
+    public SyncKP(SharkEngine se, SyncKB kb, PeerSTSet allowedUsers) {
         super(se);
+        this.syncKB = kb;
+        try {
+            if(allowedUsers != null) {
+                this.allowedUsers = InMemoSharkKB.createInMemoCopy(allowedUsers);
+            }
+        }
+        catch(SharkKBException e) {
+            // cannot happen..
+        }
     }
 
-    @Override
-    public void topicAdded(SemanticTag tag) {
-
-    }
-
-    @Override
-    public void peerAdded(PeerSemanticTag tag) {
-
-    }
-
-    @Override
-    public void locationAdded(SpatialSemanticTag location) {
-
-    }
-
-    @Override
-    public void timespanAdded(TimeSemanticTag time) {
-
-    }
-
-    @Override
-    public void topicRemoved(SemanticTag tag) {
-
-    }
-
-    @Override
-    public void peerRemoved(PeerSemanticTag tag) {
-
-    }
-
-    @Override
-    public void locationRemoved(SpatialSemanticTag tag) {
-
-    }
-
-    @Override
-    public void timespanRemoved(TimeSemanticTag tag) {
-
-    }
-
-    @Override
-    public void predicateCreated(SNSemanticTag subject, String type, SNSemanticTag object) {
-
-    }
-
-    @Override
-    public void predicateRemoved(SNSemanticTag subject, String type, SNSemanticTag object) {
-
-    }
-
-    @Override
-    public void tagChanged(SemanticTag tag) {
-
-    }
-
-    @Override
-    public void contextPointAdded(ContextPoint cp) {
-
-    }
-
-    @Override
-    public void cpChanged(ContextPoint cp) {
-
-    }
-
-    @Override
-    public void contextPointRemoved(ContextPoint cp) {
-
+    public SyncKP(SharkEngine se, SyncKB kb) {
+        this(se, kb, null);
     }
 
     @Override
     protected boolean handleRaw(ASIPInMessage message, ASIPConnection connection, InputStream inputStream) {
         return false;
     }
+  
+    // test
+    public void saveLastSeen() {
+//    private void saveLastSeen() {
+        //test
+        PeerSemanticTag p = InMemoSharkKB.createInMemoPeerSemanticTag("Alice", "http://alice.de", "mail://alice@alice.de");
+        Long t = System.currentTimeMillis();
+        this.lastSeen.put(p, t);
+        // end test
+                
+        StringBuilder buf = new StringBuilder();
+        Iterator<PeerSemanticTag> peerIter = this.lastSeen.keySet().iterator();
+        try {
+            while(peerIter.hasNext()) {
+                PeerSemanticTag peer = peerIter.next();
+                Long lastseen = this.lastSeen.get(peer);
+                buf.append(ASIPSerializer.serializeTag(peer));
+                buf.append(buf.toString());
+            }
+            this.syncKB.setProperty(LAST_SEEN_PROPERTY_NAME, buf.toString());
+        } catch (JSONException | SharkKBException ex) {
+            L.e("couldn't write last seen entries for sync - critical", this);
+        }
+    }
+    
+    /**
+     * TODO
+     */
+    private void restoreLastSeen2() {
+        try {
+            String prop = this.syncKB.getProperty(LAST_SEEN_PROPERTY_NAME);
+            
+            // TODO
+            
+        } catch (SharkKBException ex) {
+            L.d("cannot read last seen entries for sync - critical", this);
+        }
+        
+        
+        
+    }
+    
+    
 }
---
+++ src/test/java/net/sharkfw/knowledgeBase/sync/SyncKPTest.java
@@ -0,0 +1,42 @@
+package net.sharkfw.knowledgeBase.sync;
+
+import net.sharkfw.knowledgeBase.SharkKB;
+import net.sharkfw.knowledgeBase.SharkKBException;
+import net.sharkfw.knowledgeBase.inmemory.InMemoSharkKB;
+import net.sharkfw.peer.J2SEAndroidSharkEngine;
+import net.sharkfw.peer.SharkEngine;
+import net.sharkfw.system.L;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author thsc
+ */
+public class SyncKPTest {
+    
+    public SyncKPTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+     @Test
+     public void syncKPTest() throws SharkKBException {
+         SharkEngine se = new J2SEAndroidSharkEngine();
+         SharkKB kb = new InMemoSharkKB();
+         SyncKB synckb = new SyncKB(kb);
+         SyncKP kp = new SyncKP(se, synckb);
+
+         L.setLogLevel(L.LOGLEVEL_ALL);
+         kp.saveLastSeen();
+     
+     }
+}
