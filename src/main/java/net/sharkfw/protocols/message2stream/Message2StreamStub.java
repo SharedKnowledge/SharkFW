136151a137b7c04f1c42d002438d05ec6aac84be
thsc42 <thomas.schwotzer@htw-berlin.de>
2017-01-17 11:06:44 +0100

    start re-implementing that marvelous message to stream stop - love it :)

---
+++ src/main/java/net/sharkfw/protocols/message2stream/Message2StreamStub.java
@@ -0,0 +1,71 @@
+package net.sharkfw.protocols.message2stream;
+
+import net.sharkfw.asip.ASIPSpace;
+import net.sharkfw.knowledgeBase.Knowledge;
+import net.sharkfw.protocols.MessageStub;
+import net.sharkfw.protocols.RequestHandler;
+import net.sharkfw.protocols.StreamConnection;
+import net.sharkfw.protocols.StreamStub;
+import net.sharkfw.system.SharkNotSupportedException;
+
+import java.io.IOException;
+
+/**
+ * Created by local on 17.01.17.
+ */
+public class Message2StreamStub implements StreamStub, RequestHandler {
+    @Override
+    public void setHandler(RequestHandler handler) {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void start() throws IOException {
+
+    }
+
+    @Override
+    public boolean started() {
+        return false;
+    }
+
+    @Override
+    public void offer(ASIPSpace interest) throws SharkNotSupportedException {
+
+    }
+
+    @Override
+    public void offer(Knowledge knowledge) throws SharkNotSupportedException {
+
+    }
+
+    @Override
+    public void handleMessage(byte[] msg, MessageStub stub) {
+
+    }
+
+    @Override
+    public void handleStream(StreamConnection con) {
+
+    }
+
+    @Override
+    public void handleNewConnectionStream(StreamConnection con) {
+
+    }
+
+    @Override
+    public StreamConnection createStreamConnection(String addressString) throws IOException {
+        return null;
+    }
+
+    @Override
+    public String getLocalAddress() {
+        return null;
+    }
+}
