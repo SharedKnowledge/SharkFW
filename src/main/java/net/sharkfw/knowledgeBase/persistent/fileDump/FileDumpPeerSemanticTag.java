377351d6719bcba5a523ca3dd1259122c21cfea1
Michael Schwarz <schwarzm90@gmail.com>
2017-02-28 10:53:57 +0100

    Completed FileDumpPeerSemanticTag.

--- src/main/java/net/sharkfw/knowledgeBase/persistent/fileDump/FileDumpPeerSemanticTag.java
+++ src/main/java/net/sharkfw/knowledgeBase/persistent/fileDump/FileDumpPeerSemanticTag.java
@@ -5,24 +5,35 @@ import net.sharkfw.knowledgeBase.PeerSemanticTag;
 /**
  * Created by j4rvis on 2/27/17.
  */
-public class FileDumpPeerSemanticTag  extends FileDumpSemanticTag implements PeerSemanticTag{
+public class FileDumpPeerSemanticTag extends FileDumpSemanticTag implements PeerSemanticTag {
+
+    private final PeerSemanticTag peerSemanticTag;
+
+    public FileDumpPeerSemanticTag(FileDumpSharkKB fileDumpSharkKB, PeerSemanticTag tag) {
+        super(fileDumpSharkKB, tag);
+        peerSemanticTag = tag;
+    }
+
     @Override
     public String[] getAddresses() {
-        return new String[0];
+        return this.peerSemanticTag.getAddresses();
     }
 
     @Override
     public void setAddresses(String[] addresses) {
-
+        this.peerSemanticTag.setAddresses(addresses);
+        this.kb.persist();
     }
 
     @Override
     public void removeAddress(String address) {
-
+        this.peerSemanticTag.removeAddress(address);
+        this.kb.persist();
     }
 
     @Override
     public void addAddress(String address) {
-
+        this.peerSemanticTag.addAddress(address);
+        this.kb.persist();
     }
 }
